3e83a0dd849833d7af5522679890bb8a80383056
Mouli Sarkar <Mouli Sarkar@192.168.0.4>
2021-05-10 12:55:23 +0530

    initial commit

---
+++ src/test/java/DataDriven.java
@@ -0,0 +1,94 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class DataDriven {
+
+	public static Object main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+	FileInputStream fis = new FileInputStream("C:\\Users\\Mouli Sarkar\\Documents\\Selenium\\dataDriven.xlsx");	
+			
+   XSSFWorkbook workbook = new XSSFWorkbook(fis);
+   
+   int sheets=workbook.getNumberOfSheets();
+   Object a = null;
+for(int i=0;i<sheets;i++)
+   {
+   if(workbook.getSheetName(i).equalsIgnoreCase("testdata"))
+   {
+   XSSFSheet sheet=workbook.getSheetAt(i);
+   //Identify Testcases coloum by scanning the entire 1st row
+
+   Iterator<Row>  rows= sheet.iterator();// sheet is collection of rows
+   Row firstrow= rows.next();
+   Iterator<Cell> ce=firstrow.cellIterator();//row is collection of cells
+   int k=0;
+   int coloumn = 0;
+   while(ce.hasNext())
+   {
+   Cell value=ce.next();
+
+   if(value.getStringCellValue().equalsIgnoreCase("TestCases"))
+   {
+   coloumn=k;
+
+   }
+
+   k++;
+   }
+   System.out.println(coloumn);
+
+   ////once coloumn is identified then scan entire testcase coloum to identify purcjhase testcase row
+   while(rows.hasNext())
+   {
+
+   Row r=rows.next();
+
+   String testcaseName = null;
+if(r.getCell(coloumn).getStringCellValue().equalsIgnoreCase(testcaseName))
+   {
+
+   ////after you grab purchase testcase row = pull all the data of that row and feed into test
+
+   Iterator<Cell>  cv=r.cellIterator();
+   while(cv.hasNext())
+   {
+   Cell c= cv.next();
+   if(c.getCellType()==CellType.STRING)
+   {
+
+  // ((Object) a).add(c.getStringCellValue());
+   }
+   else{
+
+ //  ((Object) a).add(NumberToTextConverter.toText(c.getNumericCellValue()));
+
+   }
+   }
+   }
+
+
+   }
+
+   }
+   }
+   return a;
+
+   }
+
+
+   
+		   
+	}
+
+
